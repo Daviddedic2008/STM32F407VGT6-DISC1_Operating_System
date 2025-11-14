62a359d00b1bb20761c6cb8438727cd5769bc1a7
Daviddedic2008 <david.dedic.2008@gmail.com>
2025-11-13 22:29:03 -0500

    remove fluff

--- Debug/CMakeFiles/opsys.dir/Sources/font.c.obj.d
+++
@@ -1,3 +0,0 @@
-CMakeFiles/opsys.dir/Sources/font.c.obj: \
- C:/Users/david/STM32CubeIDE/workspace_1.19.0/opsys/Sources/font.c \
- C:/Users/david/STM32CubeIDE/workspace_1.19.0/opsys/Sources/font.h
--- Debug/CMakeFiles/opsys.dir/Sources/lcd/font.c.obj.d
+++
@@ -1,3 +0,0 @@
-CMakeFiles/opsys.dir/Sources/lcd/font.c.obj: \
- C:/Users/david/STM32CubeIDE/workspace_1.19.0/opsys/Sources/lcd/font.c \
- C:/Users/david/STM32CubeIDE/workspace_1.19.0/opsys/Sources/lcd/font.h
