26a8beca24cf7fc7253bb55a4ba94ada6e96ee2d
Daviddedic2008 <david.dedic.2008@gmail.com>
2025-11-20 10:46:59 -0500

    added scheduler

---
+++ Sources/general/scheduler/task.h
@@ -0,0 +1,13 @@
+/*
+ * task.h
+ *
+ *  Created on: Nov 20, 2025
+ *      Author: david
+ */
+
+#ifndef SOURCES_GENERAL_SCHEDULER_TASK_H_
+#define SOURCES_GENERAL_SCHEDULER_TASK_H_
+
+
+
+#endif /* SOURCES_GENERAL_SCHEDULER_TASK_H_ */
